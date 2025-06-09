f2ac95986d069ffea8cbdfc10377738acb202ec9
Jaremy Creechley <creechley@gmail.com>
2025-06-09 01:44:10 -0600

    fix readme

---
+++ src/sdfy/simd/sse2/shapesSse2.nim
@@ -0,0 +1,13 @@
+import std/math
+import vmath, chroma, pixie
+import nimsimd/hassimd, nimsimd/sse2
+
+import ../../sdfytypes
+import ../../shapes
+
+when defined(release):
+  {.push checks: off.}
+
+
+when defined(release):
+  {.pop.}
